e262981454aebaea7c904b695e9387811eea7e6d
sakanataro0113 <sakanataro1320@outlook.jp>
2025-05-15 15:42:38 +0900

    App.tsxのBrowserRouterを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import NewEntry from './pages/NewEntry';
 
@@ -7,12 +7,10 @@ function App() {
   const [entries, setEntries] = useState<{ title: string; content: string }[]>([]);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home entries={entries} />} />
-        <Route path="/new" element={<NewEntry setEntries={setEntries} />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home entries={entries} />} />
+      <Route path="/new" element={<NewEntry setEntries={setEntries} />} />
+    </Routes>
   );
 }
 
